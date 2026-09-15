b0708201501104a3fce1eedab4592c07bfd95b69
shlmalik00 <shlmalik00@gmail.com>
2026-09-15 05:49:21 -0800

    Add files via upload
    default

---
+++ api/config.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+  });
+}
